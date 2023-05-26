0356adcf20a866928b496b8169cf28dc9366286f
YSneve <cerg.2002@bk.ru>
2023-05-26 09:27:41 +0500

    Пузырьковая сортировка с OMP

--- ParelelProgTaskOne/ParelelProgTaskOne.cpp
+++ ParelelProgTaskOne/ParelelProgTaskOne.cpp
@@ -1,85 +1,55 @@
 ﻿#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <thread>
-#include <cmath>
+#include <omp.h>
 #include <chrono>
 
-void merge(std::vector<int>& v, int low, int mid, int high) {
-    std::vector<int> temp(high - low + 1);
-    int i = low, j = mid + 1, k = 0;
+using namespace std;
+using namespace chrono;
 
-    while (i <= mid && j <= high) {
-        if (v[i] <= v[j])
-            temp[k++] = v[i++];
-        else
-            temp[k++] = v[j++];
-    }
-    while (i <= mid) temp[k++] = v[i++];
-    while (j <= high) temp[k++] = v[j++];
-
-    for (int i = low; i <= high; i++)
-        v[i] = temp[i - low];
-}
-
-void batcher_sort(std::vector<int>& v, int low, int high) {
-    if (low < high) {
-        int mid = (low + high) / 2;
-        batcher_sort(v, low, mid);
-        batcher_sort(v, mid + 1, high);
-        merge(v, low, mid, high);
+void bubbleSort(int arr[], int n) {
+    int i, j;
+#pragma omp parallel for shared(arr) private(i, j)
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
 }
 
-void parallel_batcher_sort(std::vector<int>& v, int num_threads) {
-    int n = v.size();
-    int chunk_size = ceil((double)n / num_threads);
-    std::vector<std::thread> threads;
-
-    for (int i = 0; i < num_threads; i++) {
-        int low = i * chunk_size;
-        int high = std::min((i + 1) * chunk_size - 1, n - 1);
-        threads.push_back(std::thread(batcher_sort, std::ref(v), low, high));
-    }
-
-    for (int i = 0; i < num_threads; i++)
-        threads[i].join();
-
-    // Последний проход слияния
-    for (int i = 1; i < num_threads; i++)
-        merge(v, 0, i * chunk_size - 1, std::min((i + 1) * chunk_size - 1, n - 1));
-}
-
+void fillRandNums();
+const long int numOfNums = 200000;
+int arr[numOfNums];
 int main() {
-    setlocale(LC_ALL, "Russian");
-    std::vector<int> v;
+    
+    fillRandNums();
+    cout << "Nums Generated!" << endl;
 
-    // Инициализация генератора случайных чисел
-    srand(time(NULL));
+    std::cout << "Original array:\n";
+    for (int i = 0; i < numOfNums; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
 
-    // Заполнение вектора случайными числами
-    for (int i = 0; i < 16000000; i++) {
-        int randomNumber = rand() % 16000000;
-        v.push_back(randomNumber);
+    auto start = high_resolution_clock::now();
+    omp_set_num_threads(12);
+    bubbleSort(arr, numOfNums);
+
+    auto duration = duration_cast<milliseconds>( high_resolution_clock::now() - start);
+    
+    printf("TIme in ms: %d", duration);
+
+    return 0;
+}
+
+
+void fillRandNums() {
+    srand(198921);
+
+    for (long int i = 0; i < numOfNums; i++) {
+        arr[i] = rand() % 100000;
     }
 
-    int num_threads = 1;
-    /*
-    std::cout << "Исходный вектор: ";
-    for (int i = 0; i < v.size(); i++)
-        std::cout << v[i] << " ";
-    std::cout << std::endl;S
-    */
-
-    auto start = std::chrono::high_resolution_clock::now();
-    parallel_batcher_sort(v, num_threads);
-    auto stop = std::chrono::high_resolution_clock::now();
-
-    /*std::cout << "Отсортированный вектор: ";
-    for (int i = 0; i < v.size(); i++)
-        std::cout << i;*/
-
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-    std::cout << "Sort duration:" << duration.count() << std::endl;
 }
-        
